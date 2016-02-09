0f0c52355c54260a39fcf9342fd8e801c8c74a90
José Paradas <jparadas.b@gmail.com>
2016-02-08 23:24:21 -0300

    Agregado scope para consulta de fechas entre rangos

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,26 +2,29 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
   	@hotels = Hotel.all
-  	
+
   end
 
   def create
-    current_datein = booking_params[:datein].to_date
-    current_dateout = booking_params[:dateout].to_date
-    datein_ok = dateout_ok = false
-
-    Booking.where(:status => 1).find_each do |booking|
-      datein_ok = Time.now.between?(booking[:datein],booking[:dateout])
-      dateout_ok = current_dateout.between?(booking[:datein],booking[:dateout])
-
-      if datein_ok or dateout_ok
-        #Rebota al usuario. No procesa.
-      else
-        #Procede, envia email y guarda
-        end
-      end
-
+    if Booking.between(booking_params[:datein]),booking_params[:dateout]).any?
+      puts "hay fechas"
+    else
+      puts "no hay fechas"
     end
+    # current_datein = booking_params[:datein].to_date
+    # current_dateout = booking_params[:dateout].to_date
+    # datein_ok = dateout_ok = false
+
+    # Booking.where(:status => 1).find_each do |booking|
+    #   datein_ok = Time.now.between?(booking[:datein],booking[:dateout])
+    #   dateout_ok = current_dateout.between?(booking[:datein],booking[:dateout])
+    #
+    #   if datein_ok or dateout_ok
+    #     #Rebota al usuario. No procesa.
+    #   else
+    #     #Procede, envia email y guarda
+    #   end
+    # end
   end
 
   def edit

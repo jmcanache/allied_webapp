b76c819ab63541419aa68a1f2a69e847ec6a924e
maria <mjcanache@gmail.com>
2016-03-01 13:02:59 -0430

    actuailizado a alliancemia@aol

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -44,9 +44,9 @@ class BookingsController < ApplicationController
       
         #hotel_recipient = [hotel.email, hotel.name]
         #guest_recipient = [@booking.email, @booking.name]
-        hotel_recipient = ['gfernandez@yirehcs.com', hotel.name]
-        guest_recipient = ['gfernandez@yirehcs.com', @booking.name]
-        allied_recipient = ['gfernandez@yirehcs.com', 'Allied Hospitality']
+        hotel_recipient = ['alliancemia@aol.com', hotel.name]
+        guest_recipient = ['alliancemia@aol.com', @booking.name]
+        allied_recipient = ['alliancemia@aol.com', 'Allied Hospitality']
 
         send = [hotel_recipient, guest_recipient, allied_recipient] 
         send.each do |send_to|

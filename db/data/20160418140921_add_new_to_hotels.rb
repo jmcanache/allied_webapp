85dffe5432f943a3e7529b3bc28d7aa97a281626
maria <mjcanache@gmail.com>
2016-04-29 11:04:03 -0430

    migracion editada

--- db/data/20160418140921_add_new_to_hotels.rb
+++ db/data/20160418140921_add_new_to_hotels.rb
@@ -2,7 +2,7 @@ class AddNewToHotels < SeedMigration::Migration
   def up
   	Hotel.create!({
 	        :name => "Hampton Hotel",
-	        :email => "mjcanache@gmail.com",
+	        :email => "hamptom@email.com",
 	        :single => "10",
 	        :double => "10",
 	        :created_at => "2015-11-26 15:37:23",

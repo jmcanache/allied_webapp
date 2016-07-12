4b5bfe25652193565ab5c4f9fe50bb1bcd2e6a46
jose <canache39@gmail.com>
2016-07-11 19:29:47 -0500

    testing style in production 2

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Allied
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.enabled = true
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w( style.css )
   end
 end

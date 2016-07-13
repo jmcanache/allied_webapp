954690fc42a8a670ffdf878765fc4f20fd3a0b05
jose <canache39@gmail.com>
2016-07-13 11:48:01 -0500

    contact email working

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -11,6 +11,6 @@ class Notifier < ApplicationMailer
 
 	def contact_us(params)
 		@data = params
-		mail( :to => 'canache39@gmail.com', :subject => params[:subject])
+		mail( :to => 'm.rolo@allied-hospitality.com', :subject => params[:subject])
 	end
 end

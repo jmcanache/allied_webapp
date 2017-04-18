51a806987d3ebbab1dc459812867017616899b4d
Jose Canache <canache39@gmail.com>
2017-04-18 19:00:34 -0400

    secret_key_base test 3

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.site_key  =  ENV['RECAPTCHA_SITE_KEY']
-  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
+  config.site_key  =  '6LenlR0UAAAAACAJTUlk-XBa9LFCjP5wnZ0wwtBH'
+  config.secret_key = '6LenlR0UAAAAALScoo7He1sATjsILNeeExGzcJIp'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
